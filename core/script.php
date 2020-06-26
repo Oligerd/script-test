23c288f7fc894493cb3a736ac8725306f9cd7827
okolesnichenko <kolegnat@gmail.com>
2020-06-26 16:09:44 +0300

    separate html and php

---
+++ core/script.php
@@ -0,0 +1,35 @@
+<?php
+
+$directory = "modules";
+$enabled_modules = ['custom', 'custom_1', 'custom_2'];
+
+define('__ROOT__', dirname(dirname(__FILE__)));
+//        echo(__ROOT__);
+foreach ($enabled_modules as $value) {
+    require_once(__ROOT__ . '/script_test/modules/' . $value . '.php');
+}
+
+$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+
+$modules_names = [];
+
+foreach ($scanned_directory as $key => $item) {
+    $item = rtrim($item, ".php");
+    $modules_names[] = $item;
+}
+
+//        print_r($modules_names);
+
+$hello_world = "Hello world";
+
+foreach ($modules_names as $module_name) {
+
+    $function_name = $module_name . '_hello_world';
+//            print_r($function_name);
+    if (function_exists($function_name)) {
+        $function_name($hello_world);
+    }
+}
+
+
+
