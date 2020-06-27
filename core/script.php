e8e76722fbff59851cb6e07eb342bfffdda4124d
okolesnichenko <kolegnat@gmail.com>
2020-06-27 18:50:34 +0300

    create modules_names function

--- core/script.php
+++ core/script.php
@@ -1,26 +1,32 @@
 <?php
 
+$hello_world = "Hello world";
 $directory = "modules";
+$modules_names = [];
 $enabled_modules = ['custom', 'custom_1', 'custom_2'];
+$scanned_directory = array_diff(scandir($directory), array('..', '.'));
 
 define('__ROOT__', dirname(dirname(__FILE__)));
 
 foreach ($enabled_modules as $value) {
-    require_once(__ROOT__ . '/modules/' . $value . '.php');
+    require_once(__ROOT__ . '/modules/' . $value . '.php'); //parsing modules;
 }
 
-$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+//foreach ($scanned_directory as $item) {
+//    $item = rtrim($item, ".php");
+//    $modules_names[] = $item;
+//}
 
-$modules_names = [];
-
-foreach ($scanned_directory as $key => $item) {
-    $item = rtrim($item, ".php");
-    $modules_names[] = $item;
+function modules_names($scanned_directory) {
+    foreach ($scanned_directory as $item) {
+        $modules_names[] = rtrim($item, ".php");
+    }
 }
 
-//        print_r($modules_names);
+//modules_names($scanned_directory);
 
-$hello_world = "Hello world";
+//print_r(modules_names($scanned_directory));
+
 
 foreach ($modules_names as $module_name) {
 
